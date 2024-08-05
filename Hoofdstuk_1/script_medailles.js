a3760d157270a97066f010a66924b7d36f3010cb
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2024-08-05 21:57:20 +0200

    Typo

--- Hoofdstuk_1/script_medailles.js
+++ Hoofdstuk_1/script_medailles.js
@@ -4,7 +4,7 @@ console.log('Script loaded')
 //Medaillestand op 04/08/2024 omstreeks 18u30, we gaan even uit van een constante lijst (de Olympische spelen zijn geëindigd door noodweer)
 const topMedailleWinnaars = [
     {plaats: '1.', naam: 'China', goud: 18, zilver: 15,  brons: 10 },
-    {plaats: '2.', naam: 'Verenigde taten', goud: 16, zilver: 25, brons: 25 },
+    {plaats: '2.', naam: 'Verenigde Staten', goud: 16, zilver: 25, brons: 25 },
     {plaats: '3.', naam: 'Frankrijk', goud: 12, zilver: 14, brons: 16 },
     {plaats: '4.', naam: 'Australië', goud: 12, zilver: 9, brons: 7},
     {plaats: '5.', naam: 'Groot-Brittanië', goud: 10, zilver: 12, brons: 15}
