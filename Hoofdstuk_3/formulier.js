a25731deba53534fa2ab07b669f519b5efb6859b
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2024-08-11 17:08:08 +0200

    Toevoegen Async en awiat +Promise en consumer methods...

--- Hoofdstuk_3/formulier.js
+++ Hoofdstuk_3/formulier.js
@@ -15,3 +15,52 @@ function berekenUitHTML(){
     let gewicht = parseFloat(document.getElementById("gewicht").value);
     bmiBerekenen(lengte, gewicht, weergeefBMI)
 }
+
+function hulpNodig(params){
+    let eersteVersie= document.getElementById("hulpnodig");
+    eersteVersie.innerHTML += '<br><a href = "mailto:wesleyromain@gmail.com">Hulp Nodig? klik op deze link om de eigenaar te mailen</a>';
+}
+
+setTimeout(hulpNodig, 10000)
+
+function checkMotivatie(){
+let motivatiePromise = new Promise((resolve, reject) => {
+    let motivatie = document.getElementById("motivatie").value;
+
+    if (motivatie == "Heel veel zin") {
+        resolve("Wat fijn dat je gemotiveerd bent!")
+    }
+    else{
+        reject("Jammer dat de motivatie ontbreekt...")
+    }
+    
+})
+
+motivatiePromise.then(
+    function(boodschap){ //zal uitgevoerd worden indien resolved
+        let p = document.createElement("p")
+        p.textContent = boodschap;
+        document.getElementById("formulier").appendChild(p);
+    },
+    function(foutboodschap){ //zal uitgevoerd worden indien reject
+        let p = document.createElement("p")
+        p.textContent = foutboodschap;
+        document.getElementById("formulier").appendChild(p);
+    }
+)
+}
+
+async function controleVerificatie() {
+    let promise = new Promise((resolve, reject) => {
+        let som = document.getElementById("verificatie").value;
+
+        if (som == 8) {                         //gebaseerd op slide 33 van de cursus.
+            resolve("Verificatie geslaagd.");
+        } else {
+            reject("verificatie mislukt");
+        }
+    });
+        let resultaat = await promise;
+        alert(resultaat);
+};
+
