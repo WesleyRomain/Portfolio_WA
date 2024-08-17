25858305f6f7810ad22dc6c6802ce9677668a0b2
WesleyRomain <146979701+WesleyRomain@users.noreply.github.com>
2024-08-17 18:12:05 +0200

    Punten en komma's toegevoegd

--- Project_1/medailles.js
+++ Project_1/medailles.js
@@ -1,5 +1,5 @@
 // Nagaan of script effectieef wordt geïmplementeerd.
-console.log('Script loaded')
+console.log('Script loaded');
 // Medaillestand einde Olympische spelen, lijst van objecten.
 const topMedailleWinnaars = [
     {plaats: '1.', naam: 'Verenigde Staten', goud: 40, zilver: 44,  brons: 42 },
@@ -7,25 +7,25 @@ const topMedailleWinnaars = [
     {plaats: '3.', naam: 'Japan', goud: 20, zilver: 12, brons: 13 },
     {plaats: '4.', naam: 'Australië', goud: 18, zilver: 19, brons: 16},
     {plaats: '5.', naam: 'Frankrijk', goud: 16, zilver: 26, brons: 22}
-]
+];
 
 // Selecteren van het landen-lijst element uit de HTML.
-let lijstLanden = document.getElementById("landen-lijst")
+let lijstLanden = document.getElementById("landen-lijst");
 
 // Functie maken voor de som van de medailles.
 function som(...nummers){ // Gebruik van restoperator, weten niet hoeveel argumenten er zullen zijn.
-    let totaal_medailles = 0
+    let totaal_medailles = 0;
     for(let nummer of nummers){
-        totaal_medailles += nummer
+        totaal_medailles += nummer;
     }
-    return totaal_medailles
+    return totaal_medailles;
 }
 
 for(let land of topMedailleWinnaars){
     // Destructureren van objecten.
-    let {plaats, naam, goud, zilver, brons} = land
+    let {plaats, naam, goud, zilver, brons} = land;
     // Somfunctie toepassen.
-    let totaal = som(...[goud, zilver, brons]) // Gebruik van spreadoperator.
+    let totaal = som(...[goud, zilver, brons]); // Gebruik van spreadoperator.
    
     // Template litearals.
     let lijstItem=
@@ -38,15 +38,15 @@ for(let land of topMedailleWinnaars){
         <td>${totaal}</td>
     </tr>`;
 
-    lijstLanden.innerHTML +=  lijstItem // Rijen toevoegen aan de tabel (Elementen manipuleren).
+    lijstLanden.innerHTML +=  lijstItem; // Rijen toevoegen aan de tabel (Elementen manipuleren).
 
 }
 
 const invoer = document.getElementById("invoer"); // Selectie van elementen en toewijzen aan een variabele.
-const paragraaf = document.getElementById("herhaling")
+const paragraaf = document.getElementById("herhaling");
 
 paragraaf.innerHTML = localStorage.getItem("waarde"); // Weergave van laatste geüpdatete waarde in local storage.
-invoer.addEventListener("keyup", Weergave) // Eventlistner: bij elk loslaten van de toets, wordt functie hieronder uitgevoerd.
+invoer.addEventListener("keyup", Weergave); // Eventlistner: bij elk loslaten van de toets, wordt functie hieronder uitgevoerd.
 
 function Weergave(){
     localStorage.setItem("waarde", invoer.value); // Modificeren van de key in localStorage
